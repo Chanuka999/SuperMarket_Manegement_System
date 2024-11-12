9bc50fcb9fa4d3ed04ee68e72ba4d294dc116792
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-12 07:30:18 +0530

    update

--- src/Controller/CreateEmployee.java
+++ src/Controller/CreateEmployee.java
@@ -1,6 +1,27 @@
 package Controller;
 
+import Model.Database;
+import Model.Employee;
+
+import java.sql.SQLException;
+
 public class CreateEmployee {
+
+    public CreateEmployee(Employee e, Database database){
+        String insert = "INSERT INTO `employees`(`id`,`firstName`,`lastName`,`email`,"
+                +"`phoneNumber`,`address`,`salary`,`department`,`password`)"
+                +"VALUES('"+e.getFirstName()+"','"+e.getLastName()+"','','"+e.getEmail()
+                +"','"+e.getPhoneNumber()+"','"+e.getAddress()+"','"+e.getSalary()+"','"+e.getDepartment()+
+                "','"+e.getPassword()+"'); ";
+
+        try {
+            database.getStatement().execute(insert);
+            System.out.println("Employee created Successfully");
+
+        }catch (SQLException exception){
+            exception.printStackTrace();
+        }
+    }
 
 }
 
